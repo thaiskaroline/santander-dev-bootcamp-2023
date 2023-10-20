0aa696181337bfbbea32cbb68f5671e0d543fb71
thaiskaroline <thaiskarolinebentosilva@hotmail.com>
2023-10-20 20:49:45 -0300

    versão inicial

---
+++ src/main/java/me/dio/santanderdevbootcamp/SantanderDevBootcampApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevBootcampApplication.class, args);
+	}
+
+}
